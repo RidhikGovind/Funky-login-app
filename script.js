13c6ad3d59cb868cce76ce512b2f5fc86a0e44c4
RidhikGovind <ridzman2019@gmail.com>
2020-09-05 21:34:14 +0530

    database feature added

---
+++ script.js
@@ -0,0 +1,34 @@
+//server
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }))
+
+const database = new Datastore('database.db')
+database.loadDatabase();
+
+app.listen(port, () => console.log('hey this is working at port 3000'));
+
+
+app.post(('/sample'), (request, response) => {
+    const data = request.body;
+    console.log(data);
+
+    //inserting the data into a database
+    database.insert(data)
+
+    //sending back the response in normal JS format
+    response.json(data)
+
+})
+
+app.get(('/database'), (request, response) => {
+    database.find({}, (err, result) => {
+       
+       return response.json(result);
+    })
+})
